88a2468e7db6058e8522bb206fcf5a120732f3e4
Trashpanda70 <mattw8824@gmail.com>
2022-05-19 01:24:35 -0400

    Refactored for global variables
    Also cleaned up a couple statements. Silly me didn't understand how Ruby handled global variables before.

--- butcher.rb
+++ butcher.rb
@@ -24,11 +24,11 @@ let the player know the transaction went through)
 something along the lines of: "\PN received $\v[var]" (where var is the number you define 
 below for the given money to be stored in)
 =end
-def pbButcher()
+
 #Pokemon you cannot sell to the butcher are in array below
 #Enter Pokemon as entries with a colon followed by their name in all caps. 
 #Use a comma to separate entries. They do not need to be on a newline.
-except=[
+Except=[
 :DITTO,
 :MEWTWO,
 :ARCEUS
@@ -45,55 +45,57 @@ I have put the ranking I personally use as a starting point, but feel free to
 customize. If a type is listed twice then the lowest ranking will be used, if a
 type is not listed the butcher will not give money for it.
 =end
-level0=[:ROCK, :STEEL, :POISON, :GHOST] # level 0 array
-level1=[:ELECTRIC, :GROUND, :PSYCHIC, :ICE, :DARK] # level 1 array
-level2=[:NORMAL, :DRAGON, :BUG, :FIGHTING] # level 2 array
-level3=[:FIRE, :WATER, :GRASS, :FLYING] # level 3 array
+Level0=[:ROCK, :STEEL, :POISON, :GHOST] # level 0 array
+Level1=[:ELECTRIC, :GROUND, :PSYCHIC, :ICE, :DARK] # level 1 array
+Level2=[:NORMAL, :DRAGON, :BUG, :FIGHTING, :FAIRY] # level 2 array
+Level3=[:FIRE, :WATER, :GRASS, :FLYING] # level 3 array
 
 # Below is the variable to store the amount of money the butcher will give.
 # For me it is variable 29, but change it to whatever your game needs.
-var=29
+Var=29
 
-# Most amount of types a Pokemon can have, 2 by default
-types=2
+# Most amount of types a Pokemon can have, 2 by default. Used to search for what
+# "level" a specific Pokemon ranks in.
+Types=2
 
 # Currently only money calculation by height is supported since I am a little unsure
 # about how to get the weight. I will update with wegiht functionality if/when I figure it out
-# method=h
+# Method=h
 
 # Below are numbers to indicate the price per centimeter a Pokemon of a particular level is worth.
 # This value is to be multiplied by the height (in cm) for the final cost
 # price per centimeter for a level 1 Pokemon
-lv1Price=2
+Lv1Price=2
 # price per centimeter for a level 2 Pokemon
-lv2Price=3
+Lv2Price=3
 # price per centimeter for a level 3 Pokemon
-lv3Price=4
+Lv3Price=4
 
 # The maximum amount of money the butcher can give. Some Pokemon have large height values that might
 # yeild a higher than wanted payout. This value is meant to prevent that. If it is 0 or negative then
 # there will be no limit
-limit=2000
+Limit=2000
 
 # Start of method, end of customizing
+def pbButcher()
   pbMessage(_INTL("Hey trainer! Looking to sell a Pokémon for some quick money?"))
   pbMessage(_INTL("What Pokémon do you have for me?"))
   # Statement below is from the WonderTrade script by Black Eternity
   chosen=pbChoosePokemon(1,2,
-                         proc {|poke| !poke.egg? && !(poke.isShadow?) && !(except.include?(poke.species))
+                         proc {|poke| !poke.egg? && !(poke.isShadow?) && !(Except.include?(poke.species))
                          }) #Choose a Pokemon that is not an Egg, Shadow Pokemon, or on the exception list
   if pbGet(1)>=0
     if $Trainer.able_pokemon_count > 1 #True if Trainer has more than 1 usable Pokemon
-      len1 = level0.length > level1.length ? level0.length : level1.length
-      len2 = level2.length > level3.length ? level2.length : level3.length
+      len1 = Level0.length > Level1.length ? Level0.length : Level1.length
+      len2 = Level2.length > Level3.length ? Level2.length : Level3.length
       len = len1 > len2 ? len1 : len2 # get array with largest length
       num = -1
       for k in 0..len # iterate through entries
         # check for each type in each array if Pokemon is of that type
-        zero = pbGetPokemon(1).hasType?(level0[k]) if k < level0.length
-        one = pbGetPokemon(1).hasType?(level1[k]) if k < level1.length
-        two = pbGetPokemon(1).hasType?(level2[k]) if k < level2.length
-        three = pbGetPokemon(1).hasType?(level3[k]) if k < level3.length
+        zero = pbGetPokemon(1).hasType?(Level0[k]) if k < Level0.length
+        one = pbGetPokemon(1).hasType?(Level1[k]) if k < Level1.length
+        two = pbGetPokemon(1).hasType?(Level2[k]) if k < Level2.length
+        three = pbGetPokemon(1).hasType?(Level3[k]) if k < Level3.length
         found = 0
         # assign value based on what array type was found in
         if three
@@ -113,9 +115,7 @@ limit=2000
             break
         end
         # break if found types is number of max types
-        if found > types
-            break
-        end
+        break if found > Types
       end
       if num == -1 # Pokemon's type not found
         pbMessage(_INTL("I'm not entirely sure what to do with this"))
@@ -125,23 +125,23 @@ limit=2000
       # assign money variable and print message depending on value
       if num == 1
         pbMessage(_INTL("This will do I guess, here is your money."))
-        money += lv1Price
+        money += Lv1Price
       elsif num == 2
         pbMessage(_INTL("This will do nicely.\nHere's your pay and please come again."))
-        money += lv2Price
+        money += Lv2Price
       elsif num == 3
         pbMessage(_INTL("Very nice! I can get a lot of money from this.\nPlease do come again!"))
-        money += lv3Price
+        money += Lv3Price
       else # if num is 0 can't sell
         pbMessage(_INTL("We can't sell meat from that type of Pokémon."))
         return false
       end
       money *= pbSize(pbGetPokemon(1)) / 10 #Multiply by size in cm
-      if money > limit and limit > 0
-        money = limit
+      if money > Limit && Limit > 0
+        money = Limit
         pbMessage(_INTL("That's the most money I can give an unofficial provider such as yourself."))
       end
-      pbSet(var,money) # set chosen variable to added money
+      pbSet(Var,money) # set chosen variable to added money
       $Trainer.party.delete_at(pbGet(1)) # remove pokemon from party
       return true
     else #If the trainer has only 1 usable Pokemon
